0243dc01e2dd8d96c22da74c435e87686db50ed1
Likitha <likithasuguna2006@gmail.com>
2026-07-21 22:47:20 +0530

    Majority Element

---
+++ Array/06. majorityElement.java
@@ -0,0 +1,39 @@
+class Solution {
+    public ArrayList<Integer> findMajority(int[] arr) {
+       int candidate1 = 0, candidate2 = 0;
+       int count1 = 0, count2 = 0;
+       for(int num : arr){
+           if (num == candidate1){
+               count1++;
+           }
+           else if (num == candidate2){
+               count2++;
+           }
+           else if (count1 == 0){
+               candidate1 = num;
+               count1 = 1;
+           }
+           else if (count2 == 0){
+               candidate2 = num;
+               count2 = 1;
+           }
+           else{
+               count1--;
+               count2--;
+           }
+       }
+       count1 = 0;
+       count2 = 0;
+       for(int num : arr){
+           if(num == candidate1) count1++;
+           else if(num == candidate2) count2++;
+       }
+       ArrayList<Integer> result = new ArrayList<>();
+       int limit = arr.length/3;
+       if(count1 > limit) result.add(candidate1);
+       if(count2 > limit) result.add(candidate2);
+       Collections.sort(result);
+       return result;
+        
+    }
+}
