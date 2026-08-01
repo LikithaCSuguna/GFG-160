b3b62a8dc4d5204fdbc4e884359c721b6e7b6231
Likitha <likithasuguna2006@gmail.com>
2026-08-01 16:46:48 +0530

    Non-Overlapping Intervals

---
+++ Sorting/06. minRemoval.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int minRemoval(int intervals[][]) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int removed = 0;
+        int prevEnd = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= prevEnd) {
+                prevEnd = intervals[i][1];
+            } else {
+                removed++;
+            }
+        }
+        return removed;
+    }
+}
