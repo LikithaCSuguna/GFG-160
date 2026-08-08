e053ddb9d82e68380000d6f861955f8ad506ee41
Likitha <likithasuguna2006@gmail.com>
2026-08-08 20:45:57 +0530

    Search in a Row-Column Sorted

---
+++ Matrix/03. searchMatrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int row = 0;
+        int col = m - 1;
+
+        while (row < n && col >= 0) {
+
+            if (mat[row][col] == x) {
+                return true;
+            }
+
+            if (mat[row][col] > x) {
+                col--;       
+            } else {
+                row++;       
+            }
+        }
+
+        return false;
+    }
+}
