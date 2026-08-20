746e55d1e9d89fb5e7b6b89317a8b9e85ccf8974
Likitha <likithasuguna2006@gmail.com>
2026-08-20 22:57:31 +0530

    Count Subarrays with given XOR

---
+++ Hashing/09. subarrayXor.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public long subarrayXor(int arr[], int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int prefixXor = 0;
+        long count = 0;
+        for (int num : arr) {
+            prefixXor ^= num;
+            int required = prefixXor ^ k;
+            if (map.containsKey(required)) {
+                count += map.get(required);
+            }
+            map.put(prefixXor, map.getOrDefault(prefixXor, 0) + 1);
+        }
+        return count;
+    }
+}
